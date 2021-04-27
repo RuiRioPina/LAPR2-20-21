8c8065e7fffdba61fb479ab556e6bc88febdb996
RuiRioPina <1201568@isep.ipp.pt>
2021-04-27 11:50:46 +0100

    Added the addClientRole method on the Class Client and the method generatePassword which only generates a password with only lowercase letters[UC-03][Implementation]

--- src/main/java/app/domain/model/Client.java
+++ src/main/java/app/domain/model/Client.java
@@ -1,20 +1,25 @@
 package app.domain.model;
 
+import auth.AuthFacade;
+import java.util.concurrent.ThreadLocalRandom;
 public class Client {
     private int ccn;
     private int nhsNumber;
     private int birthDate;
-    private String sex;
     private int tin;
     private int phoneNumber;
+    private String sex;
+    private String email;
     private String name;
+    private String password;
 
 
-    public Client(int ccn, int nhsNumber, int birthDate, String sex, int tin, int phoneNumber, String name) {
+    public Client(int ccn, int nhsNumber, int birthDate, String sex, String email, int tin, int phoneNumber, String name) {
         this.ccn = ccn;
         this.nhsNumber = nhsNumber;
         this.birthDate = birthDate;
         this.sex = sex;
+        this.email = email;
         this.tin = tin;
         this.phoneNumber = phoneNumber;
         this.name = name;
@@ -76,6 +81,43 @@ public class Client {
         this.name = name;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void generatePassword() {
+        password="";
+        char c='a';
+        int min = 0;
+        int max = 25;
+        int random;
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < 10; i++) {
+            random = ThreadLocalRandom.current().nextInt(min, max + 1);
+            c+=random;
+            password= String.valueOf(stringBuilder.append(c));
+            c='a';
+        }
+
+
+    }
+    public void addClientRole() {
+        AuthFacade authFacade = new AuthFacade();
+        authFacade.addUserRole("Client","This is the Client that has access to his results");
+        authFacade.addUserWithRole(name,email,password,"Client");
+    }
 
 
 }
