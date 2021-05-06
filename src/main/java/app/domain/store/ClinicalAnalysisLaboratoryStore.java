5aa60fc25eb914d2463614d993c602294eeee975
Santiago Azevedo <1201623@isep.ipp.pt>
2021-05-07 00:08:49 +0100

    Adding class ClinicalAnalysisLaboratoryStore[US-08]

---
+++ src/main/java/app/domain/store/ClinicalAnalysisLaboratoryStore.java
@@ -0,0 +1,138 @@
+package app.domain.store;
+
+import app.domain.model.ClinicalAnalysisLaboratory;
+import app.domain.model.TestType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClinicalAnalysisLaboratoryStore {
+    private List<ClinicalAnalysisLaboratory> clinicalAnalysisLaboratories;
+
+    public ClinicalAnalysisLaboratoryStore() {
+        this.clinicalAnalysisLaboratories = new ArrayList<>();
+    }
+
+    public ClinicalAnalysisLaboratory registerClinicalAnalysisLaboratory(String laboratoryID, String name, String adress, long phoneNumber, long tin, List<TestType> testTypes) {
+        return new ClinicalAnalysisLaboratory(laboratoryID, name, adress, phoneNumber, tin, testTypes);
+    }
+
+    public void validateClinicalAnalysisLaboratory(ClinicalAnalysisLaboratory cal) throws IllegalArgumentException {
+        checkNameRules(cal.getName());
+        checkLaboratoryIDRules(cal.getLaboratoryID());
+        checkAdressRules(cal.getAdress());
+        checkPhoneNumberRules(cal.getPhoneNumber());
+        checkTINRules(cal.getTin());
+        checkTestTypesRules(cal.getTestTypes());
+    }
+
+    public void saveClinicalAnalysisLaboratory(ClinicalAnalysisLaboratory cal) throws IllegalArgumentException {
+        validateClinicalAnalysisLaboratory(cal);
+        addClinicalAnalysisLaboratory(cal);
+    }
+
+    private void addClinicalAnalysisLaboratory(ClinicalAnalysisLaboratory cal) {
+        this.clinicalAnalysisLaboratories.add(cal);
+    }
+
+
+
+    private void checkNameRules(String name) throws IllegalArgumentException{
+        if ((name.length()) < 1 || name.length() > 20){
+            throw new IllegalArgumentException("Name must have 1 to 20 chars.");
+        }
+
+    }
+
+    private void checkLaboratoryIDRules(String laboratoryID) throws IllegalArgumentException{
+        if (laboratoryID.length() != 5 ){
+            throw new IllegalArgumentException("LaboratoryID must have 5 chars.");
+        }
+        if (!laboratoryID.matches("[A-Za-z0-9]+")) {
+            throw new IllegalArgumentException("Code must be alphanumeric.");
+        }
+        if (this.getRepeatedLabID(laboratoryID) != null) {
+            throw new IllegalArgumentException("LaboratoryID already exist.");
+        }
+
+    }
+
+    private void checkAdressRules(String adress) throws IllegalArgumentException{
+        if ((adress.length()) < 1 || adress.length() > 30){
+            throw new IllegalArgumentException("Adress must have 1 to 30 chars.");
+        }
+        if (this.getRepeatedAdress(adress) != null) {
+            throw new IllegalArgumentException("Adress already exist.");
+        }
+    }
+
+    private void checkPhoneNumberRules(long phoneNumber) throws IllegalArgumentException{
+        if (digits(phoneNumber) != 11) {
+            throw new IllegalArgumentException("Phone number must have 11 digits.");
+        }
+        if (this.getRepeatedPhoneNumber(phoneNumber) != null) {
+            throw new IllegalArgumentException("Phone number already exist.");
+        }
+    }
+
+    private void checkTINRules(long tin) throws IllegalArgumentException{
+        if (digits(tin) != 10){
+            throw new IllegalArgumentException("TIN number must have 10 digits.");
+            }
+        if (this.getRepeatedTIN(tin) != null) {
+            throw new IllegalArgumentException("TIN already exist.");
+        }
+     }
+
+    private void checkTestTypesRules(List<TestType> testTypes) throws IllegalArgumentException {
+        if (testTypes.isEmpty()) {
+            throw new IllegalArgumentException("Test types cannot be empty.");
+        }
+    }
+
+    private long digits(long num){
+        int digits=0;
+        do {
+            num=num/10;
+            digits++;
+        }while (num!=0);
+        return digits;
+    }
+
+    public ClinicalAnalysisLaboratory getRepeatedLabID(String laboratoryID) {
+        for(ClinicalAnalysisLaboratory cal : this.clinicalAnalysisLaboratories) {
+            if(cal.getLaboratoryID().equals(laboratoryID)) {
+                return cal;
+            }
+        }
+        return null;
+    }
+
+    public ClinicalAnalysisLaboratory getRepeatedAdress(String adress) {
+        for(ClinicalAnalysisLaboratory cal : this.clinicalAnalysisLaboratories) {
+            if(cal.getAdress().equals(adress)) {
+                return cal;
+            }
+        }
+        return null;
+    }
+
+    public ClinicalAnalysisLaboratory getRepeatedPhoneNumber(long phoneNumber) {
+        for(ClinicalAnalysisLaboratory cal : this.clinicalAnalysisLaboratories) {
+            if(cal.getPhoneNumber()==(phoneNumber)) {
+                return cal;
+            }
+        }
+        return null;
+    }
+
+    public ClinicalAnalysisLaboratory getRepeatedTIN(long tin) {
+        for(ClinicalAnalysisLaboratory cal : this.clinicalAnalysisLaboratories) {
+            if(cal.getTin()==(tin)) {
+                return cal;
+            }
+        }
+        return null;
+    }
+
+}
