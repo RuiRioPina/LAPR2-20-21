69cef2d42c3de624b827eaceb77ff5d6271f230e
Santiago Azevedo <1201623@isep.ipp.pt>
2021-05-07 23:29:56 +0100

    Adding class ClinicalAnalysisLaboratoryController

---
+++ src/main/java/app/controller/ClinicalAnalysisLaboratoryController.java
@@ -0,0 +1,46 @@
+package app.controller;
+
+import app.domain.model.ClinicalAnalysisLaboratory;
+import app.domain.model.Company;
+import app.domain.model.TestType;
+import app.domain.store.ClinicalAnalysisLaboratoryStore;
+import app.domain.store.TestTypeStore;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ClinicalAnalysisLaboratoryController {
+    private Company company;
+    private ClinicalAnalysisLaboratory clinicalAnalysisLaboratory;
+
+    public ClinicalAnalysisLaboratoryController(){
+        this.company =App.getInstance().getCompany();
+    }
+
+    public List<TestType> getTestTypes() {
+        TestTypeStore ts = this.company.getTestTypeStore();
+        return ts.getTestTypes();
+    }
+
+    public ClinicalAnalysisLaboratory registerClinicalAnalysisLaboratory(String laboratoryID, String name, String adress, long phoneNumber, long tin, List<String> testTypeCodes) throws IllegalArgumentException{
+        TestTypeStore ts=this.company.getTestTypeStore();
+
+        List<TestType> testTypes = new ArrayList<TestType>();
+        for(String testTypeCode : testTypeCodes) {
+            testTypes.add(ts.getTestTypeByCode(testTypeCode));
+        }
+        ClinicalAnalysisLaboratoryStore cs = this.company.getClinicalAnalysisLaboratoryStore();
+        ClinicalAnalysisLaboratory cal = cs.registerClinicalAnalysisLaboratory(laboratoryID, name, adress, phoneNumber, tin, testTypes);
+        cs.validateClinicalAnalysisLaboratory(cal);
+
+        this.clinicalAnalysisLaboratory = cal;
+        return this.clinicalAnalysisLaboratory;
+    }
+
+    public void saveClinicalAnalysisLaboratory() throws IllegalArgumentException {
+        ClinicalAnalysisLaboratoryStore cs = this.company.getClinicalAnalysisLaboratoryStore();
+        cs.saveClinicalAnalysisLaboratory(this.clinicalAnalysisLaboratory);
+    }
+
+}
