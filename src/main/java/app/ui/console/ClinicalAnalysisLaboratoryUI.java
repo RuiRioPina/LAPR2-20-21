bc419791a2c1753d35487f1c966efc7690cbcf22
Santiago Azevedo <1201623@isep.ipp.pt>
2021-05-08 11:00:47 +0100

    Adding class ClinicalAnalysisLaboratoryUI

--- src/main/java/app/ui/console/ClinicalAnalysisLaboratoryUI.java
+++ src/main/java/app/ui/console/ClinicalAnalysisLaboratoryUI.java
@@ -1,8 +1,12 @@
 package app.ui.console;
 
 import app.controller.ClinicalAnalysisLaboratoryController;
-import app.controller.TestTypeController;
+import app.domain.model.ClinicalAnalysisLaboratory;
+import app.domain.model.TestType;
+import app.ui.console.utils.Utils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class ClinicalAnalysisLaboratoryUI implements Runnable {
@@ -26,5 +30,51 @@ public class ClinicalAnalysisLaboratoryUI implements Runnable {
         long phoneNumber=x.nextLong();
         System.out.println("Insert TIN number.");
         long tin=x.nextLong();
+
+        List<String> testTypeCodes = new ArrayList<String>();
+
+        List<TestType> list=this.clinicalAnalysisLaboratoryController.getTestTypes();
+        int option=0;
+        do
+        {
+            option = Utils.showAndSelectIndex(list, "Select test types.");
+
+            if ( (option >= 0) && (option < list.size()))
+            {
+                testTypeCodes.add(list.get(option).getCode());
+                list.remove(option);
+            }
+        }
+        while (option != -1 && !list.isEmpty());
+
+        ClinicalAnalysisLaboratory cal;
+        try {
+            cal = this.clinicalAnalysisLaboratoryController.registerClinicalAnalysisLaboratory(laboratoryID, name, adress, phoneNumber, tin, testTypeCodes);
+        } catch (IllegalArgumentException ex) {
+            System.out.println(ex.getMessage());
+            return;
+        }
+
+        System.out.println("Confirmation: \n");
+        System.out.printf("-LaboratoryID: %s\n", cal.getLaboratoryID());
+        System.out.printf("-Name: %s\n", cal.getName());
+        System.out.printf("-Adress: %s\n", cal.getAdress());
+        System.out.printf("-Phone number: %s\n", cal.getPhoneNumber());
+        System.out.printf("-TIN: %s\n", cal.getTin());
+        System.out.println("-Test types: ");
+
+        for(TestType tt : cal.getTestTypes()) {
+            System.out.printf("\t%s - %s\n", tt.getCode(), tt.getParameterCategories());
+        }
+
+        if(!Utils.confirm("Confirm test type creation (s/n)?")){
+            return;
+        }
+
+        try {
+            this.clinicalAnalysisLaboratoryController.saveClinicalAnalysisLaboratory();
+        } catch (IllegalArgumentException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
