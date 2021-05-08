1cd6afc7eb426ca9ae3da4d162c10e1b052f15f7
rafal <1201566@isep.ipp.pt>
2021-05-08 15:07:27 +0100

    Implementing Tests [UC-07] [Testing]

--- src/test/java/app/domain/model/EmployeeTest2.java
+++ src/test/java/app/domain/model/EmployeeTest2.java
@@ -233,6 +233,15 @@ assertNotEquals(e1.generateEmployeePassword().length(),notExpectedLength);
         assertFalse(e8.validateEmployee());
 
     }
+    @Test
+    public void getRole(){
+        Role r1=new Role("Receives Clients","Receptionist","Rec");
+        Employee e1= new Employee("Tiago Santa Cruz","Porto","1234",999999999L,"tiagosantacruz@gmail.com","Tiago",2,r1,null);
+        Role expected = new Role("Receives Clients","Receptionist","Rec");
+        Role notExpected = new Role("Receives Clients","Receptionist","Rec");
+        assertEquals(e1.getRole(),expected);
+        assertNotEquals(e1.getRole(),notExpected);
+    }
 
 
 }
