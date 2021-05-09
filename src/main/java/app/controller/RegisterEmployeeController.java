5a2367adbdb75a841c5826a79b4be2b469740f9e
rafal <1201566@isep.ipp.pt>
2021-05-09 14:30:13 +0100

    Added Javadoc[UC-07]

--- src/main/java/app/controller/RegisterEmployeeController.java
+++ src/main/java/app/controller/RegisterEmployeeController.java
@@ -13,11 +13,16 @@ import auth.AuthFacade;
 import java.util.List;
 
 public class RegisterEmployeeController {
+    /**
+     * Controller Class for the Register Employee function[US-07 of the integrative project of 1st year ISEP DEI students].
+     */
     private Company company;
     private AuthFacade authFacade = new AuthFacade();
 
 
-
+    /**
+     * Constructor of the Controller Class. Checks to see if the user is an administator, and gets the company that is using the software.
+     */
     public RegisterEmployeeController() {
         if (!isUserLoggedInAdmin()){
             System.out.println("You do not have admin permissions.");
@@ -25,33 +30,48 @@ public class RegisterEmployeeController {
         this.company = App.getInstance().getCompany();
     }
 
+    /**
+     * Gets the Role list of the company that is using the software
+     * @return List with roles.
+     */
     public List<Role> getlRole() {
         RoleStore lRole = this.company.getRoleStore();
         return lRole.getlRole();
     }
 
+    /**
+     * Creates an object of the employee Class.
+     * @param name- name of the employee.
+     * @param adress- adress of the employee.
+     * @param SOC- Standard Occupational Classificational Code.
+     * @param phoneNumber- phone number of the employee.
+     * @param email- email of the employee.
+     * @param userName- userName of the employee.
+     * @param nEmployees- the number of employees that exist(used to generate the id).
+     * @param role- Role of the employee in the system
+     * @param specialistDoctorIndexNumber- if the employee is a specialist doctor it contains their doctor index number if not, if not the value is null
+     * @return- Object of the employee Class.
+     */
     public Employee createEmployee(String name, String adress, String SOC, long phoneNumber, String email, String userName, int nEmployees, Role role, String specialistDoctorIndexNumber) {
         EmployeeStore lEmployee = this.company.getEmployeeStore();
         return lEmployee.createEmployee(name, adress, SOC, phoneNumber, email, userName, nEmployees, role,specialistDoctorIndexNumber);
     }
 
-    public void validateSOCInput(String SOC){
-        if (Utils.validateSOC(SOC)){
-            throw new IllegalArgumentException("SOC must have 4 digits and only numbers");
-        }
-    }
-
-    public void validateNameInput(String name){
-        if (Utils.nameContainsDigits(name)){
-            throw new IllegalArgumentException("The name shouldn't have numbers");
-        }
-
-    }
+    /**
+     * Method used to direct the saving of an employee to the employee list.
+     * @param e- Employee to be saved
+     */
     public void saveEmployee(Employee e){
        EmployeeStore lEmployee=this.company.getEmployeeStore();
        lEmployee.saveEmployee(e);
 
     }
+
+    /**
+     * Method used to link the name of the current employee classes to their index number in the role list.
+     * @param s- String that is the same as the role's shortname.
+     * @return The index number of the desired role.
+     */
     public  int getRoleIndex(String s){
         switch (s){
             case "REC":
@@ -69,6 +89,11 @@ public class RegisterEmployeeController {
 
 
     }
+
+    /**
+     * Checks to see if the user is an administrator
+     * @return boolean value telling if the user is an admin or not.
+     */
     private boolean isUserLoggedInAdmin() {
 
         return !authFacade.getCurrentUserSession().isLoggedInWithRole(Constants.ROLE_ADMIN);
--- src/main/java/app/domain/model/Employee.java
+++ src/main/java/app/domain/model/Employee.java
@@ -8,7 +8,9 @@ import java.util.Objects;
 import java.util.Random;
 
 public class Employee {
-
+    /**
+     * Object oriented Class to the describe an employee in a company context.
+     */
     private static final int PASSWORD_DIGIT_AMOUNT=2;
     private static final int PASSWORD_UPPERCASE_AMOUNT=3;
     private static final int PASSWORD_LOWERCASE_AMOUNT=2;
@@ -24,18 +26,34 @@ public class Employee {
     private String specialistDoctorIndexNumber;
 
 
-
-
-
+    /**
+     * Returns the role of the employee.
+     * @return role of the employee
+     */
     public Role getRole() {
         return role;
     }
 
+    /**
+     * Sets the employee's role
+     * @param- new role for the employee
+     */
     public void setRole(Role role) {
         this.role = role;
     }
 
-
+    /**
+     * Constructor for the employee.
+     * @param name- name of the employee.
+     * @param adress- adress of the employee.
+     * @param SOC- Standard Occupational Classificational Code.
+     * @param phoneNumber- phone number of the employee.
+     * @param email- email of the employee.
+     * @param userName- userName of the employee.
+     * @param nEmployees- the number of employees that exist(used to generate the id).
+     * @param role- Role of the employee in the system
+     * @param specialistDoctorIndexNumber- if the employee is a specialist doctor it contains their doctor index number if not, if not the value is null
+     */
     public Employee(String name, String adress, String SOC, long phoneNumber, String email, String userName, int nEmployees, Role role, String specialistDoctorIndexNumber){
         this.name=name;
         this.adress=adress;
@@ -48,6 +66,11 @@ public class Employee {
         this.role= role;
         this.specialistDoctorIndexNumber=specialistDoctorIndexNumber;
     }
+
+    /**
+     * Generates the password for the user created for the employee.
+     * @return a String with a certain amount of uppercase letters,lowercase letters and digits.
+     */
     public String generateEmployeePassword() {
         StringBuilder s = new StringBuilder();
         for (int i=0;i<PASSWORD_DIGIT_AMOUNT;i++){
@@ -66,46 +89,91 @@ public class Employee {
         return str;
     }
 
+    /**
+     * Getter for the employee's id
+     * @return String with the employee's ID
+     */
     public String getID() {
         return ID;
     }
 
+    /**
+     * Sets  the employee's ID
+     * @param ID- String with the new ID of the employee
+     */
     public void setID(String ID) {
         this.ID = ID;
     }
 
+    /**
+     * Getter for the StandardOccupationalClassification Code
+     * @return String with the SOC Code.
+     */
     public String getSOC() {
         return SOC;
     }
 
+    /**
+     * Sets the employee's StandardOccupationalClassification Code
+     * @param SOC- String with the new SOC Code.
+     */
     public void setSOC(String SOC) {
         this.SOC = SOC;
     }
 
+    /**
+     * Getter for the employee's phone number
+     * @return long with the phone number of the employee
+     */
     public long getPhoneNumber() {
         return phoneNumber;
     }
 
+    /**
+     * Sets the employee's phone number.
+     * @param phoneNumber-long wit the new phoneNumber.
+     */
+
     public void setPhoneNumber(long phoneNumber) { this.phoneNumber = phoneNumber; }
 
+    /**
+     * Getter for the employee's name
+     * @return String with the employee's name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Setter for the employee's name.
+     * @param name- String with the new name for the employee.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
 
-
+    /**
+     * Getter for the employee's adress.
+     * @return String with the adress for the employee.
+     */
     public String getAdress() {
         return adress;
     }
 
+    /**
+     * Setter for the employee's adress.
+     * @param adress-String with the new adress.
+     */
     public void setAdress(String adress) {
         this.adress = adress;
     }
 
+    /**
+     * Generates the ID of the employee using the number of employees in the company and the employee's name.
+     * @param nEmployees- total number of the employees in the company.
+     * @return String with the employee's id.
+     */
  private String generateID(int nEmployees){
         String id="";
         String employeeName = this.name;
@@ -119,38 +187,80 @@ public class Employee {
         return id;
 }
 
+    /**
+     * Getter for the specialist doctor index number.
+     * @return String with the specialist doctor index number.
+     */
     public String getSpecialistDoctorIndexNumber() {
         return specialistDoctorIndexNumber;
     }
 
+    /**
+     * Setter for the specialist doctor index number.
+     * @param specialistDoctorIndexNumber-String with the  new specialist doctor index number.
+     */
     public void setSpecialistDoctorIndexNumber(String specialistDoctorIndexNumber) {
         this.specialistDoctorIndexNumber = specialistDoctorIndexNumber;
     }
 
+    /**
+     * Getter for the employee's userName.
+     * @return String with the employee's userName
+     */
     public String getUserName() {
         return userName;
     }
 
+    /**
+     * Setter for the userName of the employee.
+     * @param userName- String with the new userName for the employee.
+     */
     public void setUserName(String userName) {
         this.userName = userName;
     }
 
+    /**
+     * Getter for the employee's password.
+     * @return String with the employee's password.
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Setter for the employee's password.
+     * @param password- String with the new password.
+     */
     public void setPassword(String password) {
         this.password = password;
     }
+
+    /**
+     * Getter for the employee's email.
+     * @return String with the employee's email.
+     */
 
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Setter for the employee's email.
+     * @param email- String with the new email.
+     */
+
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * New equals method for the Employee Class.Checks to see if the object has the same memory adress or not.
+     * Then checks to see if the other object is null.
+     * Then checks to see if the other object has the same class.
+     * Then compares every parameter of the two employees to see if they are equal and returns true if that is the case.
+     * @param o- Another Object.
+     * @return boolean value telling if the object is equal or not to this a certain employee.
+     */
     @Override
 public boolean equals(Object o){
     if (this == o) {
@@ -168,15 +278,28 @@ public boolean equals(Object o){
 }
 
 
-
-@Override
+    /**
+     * New toString method for the employee class.
+     * @return String with a formatted String presenting the employee information.
+     */
+    @Override
     public String toString(){
         if (specialistDoctorIndexNumber==null){
             return String.format("This employee is named "+this.name+". Their ID is "+ this.ID +". Their adress is "+ this.adress+". Their phone number is "+ this.phoneNumber+". \nTheir SOC is "+this.SOC+". Their email adress is "+ this.email+". Their username is "+ this.userName+". Their password is "+this.password+". Their role is "+ role.getRoleID()+".");
         }else
         return String.format("This employee is named "+this.name+". Their ID is "+ this.ID +". Their adress is "+ this.adress+". Their phone number is "+ this.phoneNumber+". \nTheir SOC is "+this.SOC+". Their email adress is " +this.email + ". Their username is "+ this.userName+". Their password is "+this.password+". Their role is "+ role.getRoleID()+". Their doctor Index number is "+this.specialistDoctorIndexNumber);
 }
-public  boolean validateEmployee(){
+
+    /**
+     * Checks to see if the employee has valid date within this program's context.
+     * Checks to see if the name has more than 35 characters or has numbers.
+     * Checks to see if the SOC code has 4 digits.
+     * Checks to see if the employee has a role.
+     * Checks to see if the specialist doctor index exists and has 6 digits.
+     * If all of the above are true this returns true.
+     * @return- boolean value that tells if the employee has valid data.
+     */
+    public  boolean validateEmployee(){
         if (name.length()>35||Utils.nameContainsDigits(name)){
             return false;
     }
@@ -191,7 +314,13 @@ public  boolean validateEmployee(){
     }
     return true;
 }
-private Character randomCharacter(String s){
+
+    /**
+     * Used to create the password, returns a random character from a String parameter.
+     * @param s- String with the characters to choose from.
+     * @return Random Character from the String
+     */
+    private Character randomCharacter(String s){
     Random randomNumber= new Random();
     int randomNo=randomNumber.nextInt(s.length());
     return s.charAt(randomNo);
