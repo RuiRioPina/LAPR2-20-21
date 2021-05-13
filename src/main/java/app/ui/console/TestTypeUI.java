77dab29abe97f26d88c269bc05a05ee3d55c94e2
JG <JG@DESKTOP-19SMORH.mshome.net>
2021-05-13 18:25:45 +0100

    Creating the UI validation of the test type [US-9]

--- src/main/java/app/ui/console/TestTypeUI.java
+++ src/main/java/app/ui/console/TestTypeUI.java
@@ -19,30 +19,65 @@ public class TestTypeUI implements Runnable {
 	
 	public void run() {
 		Scanner x = new Scanner(System.in);
-		System.out.println("Beginning to register a new type of Test.\n");
-		System.out.println("Insert the code of the test type.");
-		String code = x.nextLine();
-		System.out.println("Insert the description of the test type.");
-		String description = x.nextLine();
-		System.out.println("Insert the collecting method of the test type.");
-		String collectingMethod = x.nextLine();
-		
+		String code = "";
+		String description = "";
+		String collectingMethod = "";
 		List<String> parameterCategoryCodes = new ArrayList<String>();
+		boolean validation = false;
 		
-		List<ParameterCategory> list = this.testTypeController.getParameterCategories();
-		int option = 0;
-        do
-        {
-            option = Utils.showAndSelectIndex(list, "Select parameter category.");
-
-            if ( (option >= 0) && (option < list.size()))
-            {
-                parameterCategoryCodes.add(list.get(option).getCode());
-                list.remove(option);
-            }
-        }
-        while (option != -1 && !list.isEmpty());
-        
+		System.out.println("Beginning to register a new type of Test.\n");
+		
+		while(validation == false) {
+			System.out.println("Insert the code of the test type.");
+			code = x.nextLine();
+			validation = checkCodeRules(code);
+			if(validation == false) {
+				System.out.println("The code is invalid");
+			}
+		}
+		validation = false;
+		
+		while(validation == false) {
+			System.out.println("Insert the description of the test type.");
+			description = x.nextLine();
+			validation = checkDescriptionRules(description);
+			if(validation == false) {
+				System.out.println("The description is invalid");
+			}
+		}
+		validation = false;
+		
+		while(validation == false) {
+			System.out.println("Insert the collecting method of the test type.");
+			collectingMethod = x.nextLine();
+			validation = checkCollectingMethodRules(collectingMethod);
+			if(validation == false) {
+				System.out.println("The collecting method is invalid");
+			}
+		}
+		validation = false;
+		
+		while(validation == false) {
+			List<ParameterCategory> list = this.testTypeController.getParameterCategories();
+			int option = 0;
+	        do
+	        {
+	            option = Utils.showAndSelectIndex(list, "Select parameter category.");
+	
+	            if ( (option >= 0) && (option < list.size()))
+	            {
+	                parameterCategoryCodes.add(list.get(option).getCode());
+	                list.remove(option);
+	            }
+	        }
+	        while (option != -1 && !list.isEmpty());
+	        validation = checkParameterCategoryRules(parameterCategoryCodes);
+	        if(validation == false) {
+				System.out.println("The Parameter Category list is invalid");
+			}
+		}
+		validation = false;
+		
         TestType tt;
         try {
         	tt = this.testTypeController.createTestType(code, description, collectingMethod, parameterCategoryCodes);
@@ -69,5 +104,65 @@ public class TestTypeUI implements Runnable {
         } catch (IllegalArgumentException ex) {
         	System.out.println(ex.getMessage());
         }
+	}
+	
+	/**
+     * Checks test type code rules.
+     * @param code - code of the test type.
+     * @return validation - the validation
+     */
+	private boolean checkCodeRules(String code) {
+		boolean validation = true;
+        if (code.length() != 5) {
+        	validation = false;
+        }
+        
+        if (!code.matches("^[a-zA-Z0-9]*$")) {
+        	validation = false;
+        }
+        
+        return validation;
+    }
+	
+	/**
+     * Checks test type description rules.
+     * @param description - description of the parameter.
+     * @return validation - the validation
+     */
+	private boolean checkDescriptionRules(String description) {
+		boolean validation = true;
+	    if ((description.length()) < 1 || description.length() > 15) {
+	        validation = false;
+	    }
+	    
+	    return validation;
+	}
+	
+	/**
+     * Checks test type collecting method rules.
+     * @param collectingMethod - collecting method of the test type.
+     * @return validation - the validation
+     */
+	private boolean checkCollectingMethodRules(String collectingMethod) {
+		boolean validation = true;
+		if ((collectingMethod.length()) < 1 || collectingMethod.length() > 20) {
+			validation = false;
+		}
+		
+		return validation;
+	}
+	
+	/**
+     * Checks test type parameter categories rules.
+     * @param parameterCategories - parameter categories of the test type.
+     * @return validation - the validation
+     */
+	private boolean checkParameterCategoryRules(List<String> parameterCategories) {
+		boolean validation = true;
+		if (parameterCategories.isEmpty()) {
+			validation = false;
+		}
+		
+		return validation;
 	}
 }
