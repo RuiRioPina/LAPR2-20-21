f6072bc83603fa21b03257d47a8799fd4bc1409f
RuiRioPina <1201568@isep.ipp.pt>
2021-05-23 18:16:12 +0100

    [PPROG] Passing the Class TestResult from the Class Diagram to the Implementation [Implementation] [US-12]

---
+++ src/main/java/app/domain/model/TestResult.java
@@ -0,0 +1,29 @@
+package app.domain.model;
+
+
+public class TestResult {
+    ReferenceValue referenceValue;
+    double result;
+    Parameter parameter;
+
+    public TestResult(Parameter parameter, double result, ReferenceValue referenceValue) {
+        this.parameter = parameter;
+        this.result = result;
+        this.referenceValue = referenceValue;
+    }
+
+    public TestResult(double result, ReferenceValue refValue) {
+        this.result = result;
+        this.referenceValue = refValue;
+    }
+
+    @Override
+    public String toString() {
+        return "ResultOfTest{" +
+                "result=" + result +
+                ", refValue=" + referenceValue +
+                '}';
+    }
+
+
+}
