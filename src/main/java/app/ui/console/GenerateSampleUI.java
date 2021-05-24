0df7ab18efba25338258b3c879e9132fa052acc4
JG <JG@DESKTOP-19SMORH.mshome.net>
2021-05-24 17:01:30 +0100

    perfecting the work [US-5]

--- src/main/java/app/ui/console/GenerateSampleUI.java
+++ src/main/java/app/ui/console/GenerateSampleUI.java
@@ -1,11 +1,26 @@
 package app.ui.console;
 
+import java.util.Scanner;
+
+import app.controller.GenerateSampleController;
+
 public class GenerateSampleUI implements Runnable {
+	
+	private GenerateSampleController generateSampleController;
+	
+	public GenerateSampleUI() {
+		this.generateSampleController = new GenerateSampleController();
+	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		Scanner x = new Scanner(System.in);
+		int numberOfSamples = 0;
 		
+		System.out.println("Beginning to generate a sample.\n");
+		
+		System.out.println("Select the number of sample to generate: ");
+		numberOfSamples = x.nextInt();
 	}
 
 }
