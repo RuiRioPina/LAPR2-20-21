4d748716d8e67512e85c8201d5450ca1a6223dcf
RuiRioPina <1201568@isep.ipp.pt>
2021-05-24 11:09:26 +0100

    [PPROG] Adding the implementation of a TestParam class only dedicated to handle functionalities of the Parameters on a Test, in order to ensure the High Cohesion, Low Coupling pattern  [Implementation] [US-3]

---
+++ src/main/java/app/domain/model/TestParam.java
@@ -0,0 +1,20 @@
+package app.domain.model;
+
+import java.util.List;
+
+public class TestParam {
+    List<Parameter> parametersSelected;
+    public TestParam(Test test) {
+        this.parametersSelected = test.getParameter();
+    }
+
+    public Parameter findParameterInTestParameter(String parameterCode) {
+        for (Parameter parameter : parametersSelected) {
+            if (parameter.getCode().equals(parameterCode)) {
+                return parameter;
+            }
+        }
+        return null; // not found
+    }
+
+}
