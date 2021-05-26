2eeba4b565ad528980327efcd5aec5ebf46c99a4
RuiRioPina <1201568@isep.ipp.pt>
2021-05-26 09:28:10 +0100

    Adding the RecordTestResultsController

---
+++ src/main/java/app/controller/RecordTestResultsController.java
@@ -0,0 +1,56 @@
+package app.controller;
+
+import app.domain.model.*;
+import app.domain.store.ResultOfTestStore;
+import app.domain.store.TestStore;
+
+import java.util.List;
+
+public class RecordTestResultsController {
+    Company company = App.getInstance().getCompany();
+    TestStore tests = App.getInstance().getCompany().getTestStore();
+
+    ResultOfTestStore resultOfTestStore = this.company.getResultOfTestStore();
+
+
+    public RecordTestResultsController() {
+        //Constructor
+    }
+
+    public boolean testExists(long barcode) {
+        return tests.testExists(barcode);
+    }
+
+    public Test getTest(long barcode) {
+        return tests.getTest(barcode);
+    }
+
+    public List<Parameter> getListOfParametersSelected(Test test) {
+        return tests.getTest(test).getParameterStore();
+    }
+
+    public void addTestResult(String parameterCode, double result) {
+        tests.addTestResult(parameterCode, result);
+    }
+
+
+    public void associateToParameter() {
+        tests.associateToParameter();
+    }
+
+
+    public List<Parameter> getParameterStoreToShow(Test test) {
+        return tests.getTest(test).getParameterStoreToShow();
+    }
+
+
+    public List<Parameter> getValidatedTests(String parameterCode) {
+        return tests.getValidatedTests(parameterCode);
+    }
+
+    public boolean hasTestPassedSampleCollection(long barcode) {
+        return tests.hasTestPassedSampleCollection(barcode);
+    }
+
+
+}
