41f01dcdd46dbfe59c79c95e76e2ec6ae1e41345
JG <JG@DESKTOP-19SMORH.mshome.net>
2021-05-27 17:23:46 +0100

    creating the possibility to have different APIs [US-5]

--- src/main/java/app/controller/BarcodeController.java
+++ src/main/java/app/controller/BarcodeController.java
@@ -1,24 +1,63 @@
 package app.controller;
 
 import java.io.File;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
 
+import app.domain.shared.Configuration;
 import net.sourceforge.barbecue.Barcode;
 import net.sourceforge.barbecue.BarcodeFactory;
 import net.sourceforge.barbecue.BarcodeImageHandler;
 
 public class BarcodeController {
 
+	private static String BasePath;
+	private static Method createUPCA;
+	private static Method setPrefferedBarHeight;
+	private static Method savePNG;
+	
+	static {
+		BasePath = System.getProperty("user.dir") + "\\Barcodes";
+		File directory = new File(BasePath);
+		if (!directory.exists()) {
+			directory.mkdir();
+		}
+		
+		try {
+			File jar = new File(Configuration.getBarcodeApiJarPath());
+    		URL url = jar.toURI().toURL();
+    		ClassLoader loader = new URLClassLoader(new URL[] {url});
+    		
+    		Class<?> cls = loader.loadClass(Configuration.getBarcodeApiCreator());
+    		createUPCA = cls.getMethod(Configuration.getBarcodeApiCreatorMethod(), String.class);
+    		
+    		Class<?> cls2 = loader.loadClass(Configuration.getBarcodeApiObject());
+        	setPrefferedBarHeight = cls2.getMethod(Configuration.getBarcodeApiObjectMethod(), int.class);
+        	
+        	Class<?> cls3 = loader.loadClass(Configuration.getBarcodeApiImage());
+        	savePNG = cls3.getMethod(Configuration.getBarcodeApiImageMethod(), cls2, File.class);
+		} catch (Exception ex) {
+			throw new RuntimeException(ex.getMessage());
+		}
+	}
+	
 	public static void generateBarcode(String barcode) throws Exception {
 		try {
-			String path = System.getProperty("user.dir");
-			File directory = new File(path + "\\Barcodes");
-			if (!directory.exists()) {
-				directory.mkdir();
-			}
-			String filePath = String.format("%s\\Barcodes\\Barcode_%s.jpeg", path, barcode);
-			Barcode bc = BarcodeFactory.createUPCA(barcode);
-        	bc.setPreferredBarHeight(100);
-        	BarcodeImageHandler.savePNG(bc, new File(filePath));
+			
+			String filePath = String.format("%s\\Barcode_%s.jpeg", BasePath, barcode);
+			
+//			If you want to use by POM
+//			Barcode bc = BarcodeFactory.createUPCA(barcode);
+//        	bc.setPreferredBarHeight(100);
+//        	BarcodeImageHandler.savePNG(bc, new File(filePath));
+        	       	
+        	Object bc2 = createUPCA.invoke(null, barcode);
+        	
+        	setPrefferedBarHeight.invoke(bc2, 100);
+        	
+        	savePNG.invoke(null, bc2, new File(filePath));
+        	
 		} catch (Exception e) {
 			throw new Exception("Error generating barcode");
 		}
---
+++ src/main/java/app/domain/shared/Configuration.java
@@ -0,0 +1,49 @@
+package app.domain.shared;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Configuration {
+
+	private static Properties props;
+	
+	static {
+		props = new Properties();
+		try {
+			InputStream in = new FileInputStream("config.properties");
+			props.load(in);
+			in.close();
+		} catch (Exception ex) {
+			throw new RuntimeException(ex.getMessage());
+		}
+	}
+	
+	public static String getBarcodeApiJarPath () {
+		return props.getProperty("BarcodeApi.jarPath");
+	}
+	
+	public static String getBarcodeApiCreator () {
+		return props.getProperty("BarcodeApi.BarcodeCreator");
+	}
+	
+	public static String getBarcodeApiObject() {
+		return props.getProperty("BarcodeApi.BarcodeObject");
+	}
+	
+	public static String getBarcodeApiImage () {
+		return props.getProperty("BarcodeApi.BarcodeImage");
+	}
+	
+	public static String getBarcodeApiCreatorMethod () {
+		return props.getProperty("BarcodeApi.BarcodeCreatorMethod");
+	}
+	
+	public static String getBarcodeApiObjectMethod () {
+		return props.getProperty("BarcodeApi.BarcodeObjectSize");
+	}
+	
+	public static String getBarcodeApiImageMethod () {
+		return props.getProperty("BarcodeApi.BarcodeImageSaving");
+	}
+}
