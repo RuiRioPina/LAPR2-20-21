49eb1e6ea42a6e2fa0cc8d6e14e5a0c08f949258
JG <JG@DESKTOP-19SMORH.mshome.net>
2021-05-27 16:07:06 +0100

    correcting the generation of barcodes [US-5]

--- src/main/java/app/controller/BarcodeController.java
+++ src/main/java/app/controller/BarcodeController.java
@@ -3,7 +3,6 @@ package app.controller;
 import java.io.File;
 
 import net.sourceforge.barbecue.Barcode;
-import net.sourceforge.barbecue.BarcodeException;
 import net.sourceforge.barbecue.BarcodeFactory;
 import net.sourceforge.barbecue.BarcodeImageHandler;
 
@@ -11,7 +10,12 @@ public class BarcodeController {
 
 	public static void generateBarcode(String barcode) throws Exception {
 		try {
-			String filePath = String.format("C:\\Users\\ruiri\\Documents\\Barcode_%s.jpeg", barcode);
+			String path = System.getProperty("user.dir");
+			File directory = new File(path + "\\Barcodes");
+			if (!directory.exists()) {
+				directory.mkdir();
+			}
+			String filePath = String.format("%s\\Barcodes\\Barcode_%s.jpeg", path, barcode);
 			Barcode bc = BarcodeFactory.createUPCA(barcode);
         	bc.setPreferredBarHeight(100);
         	BarcodeImageHandler.savePNG(bc, new File(filePath));
