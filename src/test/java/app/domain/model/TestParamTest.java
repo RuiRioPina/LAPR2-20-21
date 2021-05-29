c9965be6d3cdbe318b0bd802352aa64ea4e05129
RuiRioPina <1201568@isep.ipp.pt>
2021-05-29 10:19:50 +0100

    [PPROG] Defining Unit tests for the TestParam class. [Implementation]

--- src/test/java/app/domain/model/TestParamTest.java
+++ src/test/java/app/domain/model/TestParamTest.java
@@ -40,15 +40,15 @@ public class TestParamTest {
 
 
 
-        ps.saveParameter(new Parameter("HB011", "HB", "Haemoglobin", cat));
-        ps.saveParameter(new Parameter("WBC01", "WBC", "White Cell Count", cat));
-        ps.saveParameter(new Parameter("PLT01", "PLT", "Platelet Count", cat));
-        ps.saveParameter(new Parameter("RBC01", "RBC", "Red Blood Count", cat));
-        ps.saveParameter(new Parameter("MCV01", "MCV", "Mean Cell Volume", cat1));
-        ps.saveParameter(new Parameter("MCH01", "MCH", "Mean Cell Heaemoglob", cat1));
-        ps.saveParameter(new Parameter("MCHC1", "MCHC", "MCH Concentration", cat1));
-        ps.saveParameter(new Parameter("ESR01", "ESR", "Erythrocyte Sed Rate", cat1));
-        ps.saveParameter(new Parameter("IgGAR", "IgC", "Antibodies", cat2));
+        ps.saveParameter(new Parameter("HT011", "HB", "Haemoglobin", cat));
+        ps.saveParameter(new Parameter("WCC01", "WBC", "White Cell Count", cat));
+        ps.saveParameter(new Parameter("PTT01", "PLT", "Platelet Count", cat));
+        ps.saveParameter(new Parameter("RTC01", "RBC", "Red Blood Count", cat));
+        ps.saveParameter(new Parameter("MTV01", "MCV", "Mean Cell Volume", cat1));
+        ps.saveParameter(new Parameter("MTH01", "MCH", "Mean Cell Heaemoglob", cat1));
+        ps.saveParameter(new Parameter("MTHC1", "MCHC", "MCH Concentration", cat1));
+        ps.saveParameter(new Parameter("ETR01", "ESR", "Erythrocyte Sed Rate", cat1));
+        ps.saveParameter(new Parameter("ITGAR", "IgC", "Antibodies", cat2));
         List<Parameter> par = new ArrayList<>();
         par.add(ps.getParameters().get(0));
         par.add(ps.getParameters().get(1));
@@ -56,8 +56,8 @@ public class TestParamTest {
         par.add(ps.getParameters().get(3));
 
         TestTypeStore tts = this.company.getTestTypeStore();
-        tts.saveTestType(new TestType("BLT10", "Blood Test", "Venipuncture", pc));
-        tts.saveTestType(new TestType("CVD10", "Covid-19 Test", "Nasopharyngeal", p));
+        tts.saveTestType(new TestType("BTT10", "Blood Test", "Venipuncture", pc));
+        tts.saveTestType(new TestType("CTD10", "Covid-19 Test", "Nasopharyngeal", p));
 
         ClientList cl = this.company.getClientList();
         Client c = new Client(1234567890123456L, 1234567890, "22-01-2002", "jorge@gmail.com", 1111111111L, 22222222222L, "Jorge Ferreira");
@@ -78,5 +78,63 @@ public class TestParamTest {
 
     @Test
     public void testFindParameterInTestParameter() {
+        ParameterStore ps = this.company.getParameterStore();
+
+        List<ParameterCategory> pc = new ArrayList<>();
+        ParameterCategory p1 = new ParameterCategory("CAT00", "Category00");
+        ParameterCategory P2 = new ParameterCategory("CAT01", "Category01");
+        ParameterCategory P3 = new ParameterCategory("CAT02", "Category02");
+        pc.add(p1);
+        pc.add(P2);
+        List<ParameterCategory> p = new ArrayList<>();
+        List<ParameterCategory> cat = new ArrayList<>();
+        cat.add(pc.get(0));
+        List<ParameterCategory> cat1 = new ArrayList<>();
+        cat1.add(P2);
+
+        List<ParameterCategory> cat2 = new ArrayList<>();
+        cat2.add(P3);
+        p.add(P3);
+        pc.add(p1);
+        pc.add(P2);
+        Parameter parameter = new Parameter("PLT01", "PLT", "Platelet Count", cat);
+
+
+        ps.saveParameter(new Parameter("HB011", "HB", "Haemoglobin", cat));
+        ps.saveParameter(new Parameter("WBC01", "WBC", "White Cell Count", cat));
+        ps.saveParameter(parameter);
+        ps.saveParameter(new Parameter("RBC01", "RBC", "Red Blood Count", cat));
+        ps.saveParameter(new Parameter("MCV01", "MCV", "Mean Cell Volume", cat1));
+        ps.saveParameter(new Parameter("MCH01", "MCH", "Mean Cell Heaemoglob", cat1));
+        ps.saveParameter(new Parameter("MCHC1", "MCHC", "MCH Concentration", cat1));
+        ps.saveParameter(new Parameter("ESR01", "ESR", "Erythrocyte Sed Rate", cat1));
+        ps.saveParameter(new Parameter("IgGAR", "IgC", "Antibodies", cat2));
+        List<Parameter> part = new ArrayList<>();
+        part.add(parameter);
+        part.add(ps.getParameters().get(0));
+        part.add(ps.getParameters().get(1));
+        part.add(ps.getParameters().get(2));
+        part.add(ps.getParameters().get(3));
+
+        TestTypeStore tts = this.company.getTestTypeStore();
+        tts.saveTestType(new TestType("BLT10", "Blood Test", "Venipuncture", pc));
+        tts.saveTestType(new TestType("CVD10", "Covid-19 Test", "Nasopharyngeal", p));
+
+        ClientList cl = this.company.getClientList();
+        Client c = new Client(1234567890123456L, 1234567890, "22-01-2002", "jorge@gmail.com", 1111111111L, 22222222222L, "Jorge Ferreira");
+        cl.saveClient(c);
+
+        TestStore ts = this.company.getTestStore();
+        Date data = new Date(System.currentTimeMillis());
+
+        app.domain.model.Test testSemSample1 = new app.domain.model.Test("123454abcdef", "932992999999", c, tts.getTestTypes().get(0), pc, part, data);
+        TestParam testParam = new TestParam(testSemSample1);
+
+        Parameter actual1 = testParam.findParameterInTestParameter("PLT01",part);
+        Parameter actual2 = testParam.findParameterInTestParameter("PPP01",part);
+        Parameter expected1 = parameter;
+        assertEquals(expected1,actual1);
+        assertNull(actual2);
+
     }
 }
