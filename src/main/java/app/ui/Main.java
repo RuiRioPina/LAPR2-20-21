4a3158eaefec1099fadd321e7e896b83d1491267
JG <JG@DESKTOP-19SMORH.mshome.net>
2021-06-08 14:11:46 +0100

    Restore JavaFX changes

--- src/main/java/app/ui/Main.java
+++ src/main/java/app/ui/Main.java
@@ -1,64 +1,65 @@
 package app.ui;
 
+import app.controller.App;
 import app.ui.console.MainMenuUI;
 
-//import javafx.application.Application;
-//import javafx.event.EventHandler;
-//import javafx.fxml.FXMLLoader;
-//import javafx.scene.Parent;
-//import javafx.scene.Scene;
-//import javafx.scene.control.Alert;
-//import javafx.scene.control.Alert.AlertType;
-//import javafx.scene.control.ButtonType;
-//import javafx.stage.Stage;
-//import javafx.stage.WindowEvent;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 /**
  *
  * @author Paulo Maio [pam@isep.ipp.pt]
  */
-public class Main {
+public class Main extends Application{
 
-//	@Override
-//	public void start(Stage stage) throws Exception {
-//		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainMenuGUIScene.fxml"));
-//		Parent root = loader.load();
-//		
-//		Scene scene = new Scene(root);
-//		scene.getStylesheets().add("/styles/Styles.css");
-//	
-//		stage.setTitle("Menu Inicial");
-//		stage.setScene(scene);
-//		stage.setMaximized(true);
-//		stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-//            @Override
-//            public void handle(WindowEvent event) {
-// 
-//            }
-//        });	
-//		stage.show();
-//	}
+	@Override
+	public void start(Stage stage) throws Exception {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainMenuGUIScene.fxml"));
+		Parent root = loader.load();
+		
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add("/styles/Styles.css");
+	
+		stage.setTitle("Menu Inicial");
+		stage.setScene(scene);
+		stage.setMaximized(true);
+		stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent event) {
+            	App.getInstance().fechar(event);
+            }
+        });	
+		stage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	public void Fechar(WindowEvent event) {
+		Alert aviso = new Alert(AlertType.CONFIRMATION, "Deseja mesmo sair?", ButtonType.YES, ButtonType.NO);
+		aviso.setHeaderText("Confirma��o da a��o");
+		aviso.showAndWait();
+		ButtonType resultado = aviso.getResult();
+		if(resultado == ButtonType.NO) {
+			event.consume();
+		} 
+	}
+//    public static void main(String[] args) {
+//        try {
+//            MainMenuUI menu = new MainMenuUI();
 //
-//	public static void main(String[] args) {
-//		launch(args);
-//	}
-//	
-//	public void Fechar(WindowEvent event) {
-//		Alert aviso = new Alert(AlertType.CONFIRMATION, "Deseja mesmo sair?", ButtonType.YES, ButtonType.NO);
-//		aviso.setHeaderText("Confirma��o da a��o");
-//		aviso.showAndWait();
-//		ButtonType resultado = aviso.getResult();
-//		if(resultado == ButtonType.NO) {
-//			event.consume();
-//		} 
-//	}
-    public static void main(String[] args) {
-        try {
-            MainMenuUI menu = new MainMenuUI();
-
-            menu.run();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+//            menu.run();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
--- src/main/java/app/ui/gui/AuthGUISceneController.java
+++ src/main/java/app/ui/gui/AuthGUISceneController.java
@@ -1,53 +1,57 @@
 package app.ui.gui;
 
-//import app.controller.App;
-//import auth.AuthFacade;
-//import javafx.event.ActionEvent;
-//import javafx.fxml.FXML;
-//import javafx.scene.control.Alert;
-//import javafx.scene.control.Alert.AlertType;
-//import javafx.scene.control.Label;
-//import javafx.scene.control.TextField;
-//import javafx.stage.Window;
-//import javafx.stage.WindowEvent;
+import app.controller.App;
+import auth.AuthFacade;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Window;
+import javafx.stage.WindowEvent;
 
 public class AuthGUISceneController {
-//
-//	private MainMenuGUISceneController menuUI;
-//	private App app;
-//	
-//	@FXML
-//	private Label lblLogin;	
-//	@FXML
-//	private TextField txtLogin;
-//	@FXML
-//	private TextField txtPassword;
-//	
-//	public AuthGUISceneController() {
-//		this.app = App.getInstance();
-//	}
-//	
-//	@FXML
-//    private void menuLoginSairAction(ActionEvent event) {
-//        Window window = lblLogin.getScene().getWindow();
-//        window.fireEvent(new WindowEvent(window, WindowEvent.WINDOW_CLOSE_REQUEST));
-//    }
-//	
-//	public void associarParentUI(MainMenuGUISceneController menuUI) {
-//        this.menuUI = menuUI;
-//    }
-//	
-//	public void menuLoginConfirmarAction(ActionEvent event) {
-//        String user = txtLogin.getText();
-//        String pass = txtPassword.getText();
-//        
-//        boolean sucesso = app.doLogin(user, pass);
-//        if (!sucesso)
-//        {
-//        	Alert erro = new Alert(AlertType.ERROR, "Utilizador ou Palavra-Passe erradas.");
-//        	erro.showAndWait();
-//        	return;
-//        }
-//        menuLoginSairAction(event);
-//	}
+
+	private MainMenuGUISceneController menuUI;
+	private App app;
+	
+	@FXML
+	private Label lblLogin;	
+	@FXML
+	private TextField txtLogin;
+	@FXML
+	private TextField txtPassword;
+	
+	public AuthGUISceneController() {
+		this.app = App.getInstance();
+	}
+	
+	@FXML
+    private void menuLoginSairAction(ActionEvent event) {
+        Window window = lblLogin.getScene().getWindow();
+        window.fireEvent(new WindowEvent(window, WindowEvent.WINDOW_CLOSE_REQUEST));
+    }
+	
+	public void associarParentUI(MainMenuGUISceneController menuUI) {
+        this.menuUI = menuUI;
+    }
+	
+	public void menuLoginConfirmarAction(ActionEvent event) {
+        String user = txtLogin.getText();
+        String pass = txtPassword.getText();
+        boolean sucesso = false;
+        try {
+        	sucesso = app.doLogin(user, pass);
+        } catch(Exception ex) {
+        	if (!sucesso || user.isBlank() || pass.isBlank())
+        	{
+        		Alert erro = new Alert(AlertType.ERROR, "Utilizador ou Palavra-Passe erradas.");
+        		erro.showAndWait();
+        		return;
+        	}
+        }
+         
+        menuLoginSairAction(event);
+	}
 }
