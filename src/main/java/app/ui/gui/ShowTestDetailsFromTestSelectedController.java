183059fb5b03806beb65df3ef4fbc9c78a63b1d0
RuiRioPina <1201568@isep.ipp.pt>
2021-06-11 12:47:24 +0100

    [PPROG] US-13 UI is fully functional, just need to implement with the different sort algorithms and correct some bugs. [US-13]

--- src/main/java/app/ui/gui/ShowTestDetailsFromTestSelectedController.java
+++ src/main/java/app/ui/gui/ShowTestDetailsFromTestSelectedController.java
@@ -2,24 +2,15 @@ package app.ui.gui;
 
 import app.controller.App;
 import app.domain.model.*;
-import app.ui.gui.utils.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
 
-import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
