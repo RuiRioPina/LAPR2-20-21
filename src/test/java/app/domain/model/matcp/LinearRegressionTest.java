c5230f2a6342c3ff629761fb800d1c62befccafd
RuiRioPina <1201568@isep.ipp.pt>
2021-06-17 11:36:41 +0100

    [PPROG] Resolving an error on a test  [US-13]

--- src/test/java/app/domain/model/matcp/LinearRegressionTest.java
+++ src/test/java/app/domain/model/matcp/LinearRegressionTest.java
@@ -105,7 +105,10 @@ public class LinearRegressionTest {
         LinearRegression linearRegression = new LinearRegression(x, y);
         double[] actual = linearRegression.arrayYthroughRegressionModel();
         double[] expected = {3.075863344274421, 0.8889325505062183, 3.954220794230502, 2.08995192085433, 1.8389926494383062, 3.416450926910452, 4.958057879894595, 1.2832971198742549, 2.520167814710369, 4.474064999306551};
-        Assert.assertEquals(Arrays.toString(expected), Arrays.toString(actual));
+
+        String actualString = Arrays.toString(actual);
+        String expectedString = Arrays.toString(expected);
+        Assert.assertEquals(actualString,expectedString);
     }
 
     @Test
