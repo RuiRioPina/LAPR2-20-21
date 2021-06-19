dca832b617141923b38873da036fccff02431de4
RuiRioPina <1201568@isep.ipp.pt>
2021-06-19 21:38:58 +0100

    [PPROG] Defining tests for NHSReport [US-19]

---
+++ src/test/java/app/domain/model/NHSReportTest.java
@@ -0,0 +1,40 @@
+package app.domain.model;
+
+import app.domain.model.matcp.LinearRegression;
+import app.domain.model.matcp.MultiLinearRegression;
+import org.junit.Test;
+
+import java.util.Calendar;
+
+import static org.junit.Assert.*;
+
+public class NHSReportTest {
+
+    @Test
+    public void getReportString(){
+        double[] arrX1 = {3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+        double[] arrX2 = {825, 215, 1070, 550, 480, 920, 1350, 325, 670, 1215};
+        double[] arrY1 = {3.5, 1, 4, 2, 1, 3, 4.5, 1.5, 3, 5};
+        LinearRegression linearRegression = new LinearRegression(arrX2, arrY1);
+        MultiLinearRegression multiLinearRegression = new MultiLinearRegression(arrX1, arrX2, arrY1);
+        NHSReport nhsReport = new NHSReport(linearRegression, arrX2 ,arrY1);
+        NHSReport nhsReportMultilinear = new NHSReport(linearRegression, arrX2 ,arrY1);
+        Calendar data1 = Calendar.getInstance();
+        Calendar data2 = Calendar.getInstance();
+        data1.set(2021, Calendar.MAY, 3);
+        data2.set(2021, Calendar.MAY, 13);
+        int actual = nhsReport.getReportString(data1,data2,0.95,0,0.95,0,0.95,0.95,"days").trim().length();
+        int actual1 = nhsReportMultilinear.getReportString(data1,data2,0.95,0,0.95,0,0.95,0.95,"days").trim().length();
+        int actual2 = nhsReport.getReportString(data1,data2,0.95,0,0.95,0,0.95,0.95,"weeks").trim().length();
+        int actual3 = nhsReportMultilinear.getReportString(data1,data2,0.95,0,0.95,0,0.95,0.95,"weeks").trim().length();
+        int expected = 1765;
+        int expected1 = 1765;
+        int expected2 = 1109;
+        int expected3 = 1109;
+
+        assertEquals(expected,actual);
+        assertEquals(expected1,actual1);
+        assertEquals(expected2,actual2);
+        assertEquals(expected3,actual3);
+    }
+}
