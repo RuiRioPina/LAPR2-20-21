9830049b259dd442531477b07ce80befb9baa5b8
RuiRioPina <1201568@isep.ipp.pt>
2021-06-19 21:51:44 +0100

    [PPROG] Correcting error on test [US-19]

--- src/test/java/app/domain/model/NHSReportTest.java
+++ src/test/java/app/domain/model/NHSReportTest.java
@@ -27,7 +27,7 @@ public class NHSReportTest {
         int actual1 = nhsReportMultilinear.getReportString(data1,data2,0.95,0,0.95,0,0.95,0.95,"days").length();
         int actual2 = nhsReport.getReportString(data1,data2,0.95,0,0.95,0,0.95,0.95,"weeks").length();
         int actual3 = nhsReportMultilinear.getReportString(data1,data2,0.95,0,0.95,0,0.95,0.95,"weeks").length();
-        int expected = 1877;
+        int expected = 1739;
         int expected1 = 1980;
         int expected2 = 1111;
         int expected3 = 1214;
