0b318449f197a03487479aa908c306d42eedb89a
jorgefcferreira <1201564@isep.ipp.pt>
2021-06-20 18:09:49 +0100

    [PPROG] LabCoordinatorStore tests

--- src/test/java/app/domain/store/LabCoordinatorStoreTest.java
+++ src/test/java/app/domain/store/LabCoordinatorStoreTest.java
@@ -6,16 +6,62 @@ import app.domain.model.ParameterCategory;
 import app.domain.model.TestType;
 import org.junit.Test;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
 public class LabCoordinatorStoreTest {
-/*
+
     @Test
-    public void getNnr() {
+    public void maxSubArraySum() {
+        int [] list = new int [10];
+        list [1] = 1;
+        list [2] = 3;
+        LabCoordinatorStore lc = new LabCoordinatorStore();
+        assertNotEquals(LabCoordinatorStore.maxSubArraySum(list), LabCoordinatorStore.maxSubArraySum(list));
+    }
+
+    @Test
+    public void maxSubArray() {
+        int [] list = new int [10];
+        list [1] = 1;
+        list [2] = 3;
+        LabCoordinatorStore lc = new LabCoordinatorStore();
+        assertNotEquals(LabCoordinatorStore.maxSubArray(list), LabCoordinatorStore.maxSubArray(list));
+    }
+
+@Test
+    public void testGetNumberTestsByYear() {
+    Client c = new Client(1234567890123456L, 1234567890, "22-01-2002", "jorge@gmail.com", 1111111111L, 22222222222L, "Jorge Ferreira");
+    List<ParameterCategory> pc = new ArrayList<>();
+    ParameterCategory p1 = new ParameterCategory("CAT00", "Category00");
+    pc.add(p1);
+    TestType tt1 = new TestType("BLT00", "Blood Test", "Venipuncture", pc);
+    TestStore ts = new TestStore();
+    List<Parameter> par = new ArrayList<>();
+    Parameter par1= new Parameter("HB000", "HB", "Haemoglobin", pc);
+    par.add(par1);
+    Calendar data = Calendar.getInstance();
+    app.domain.model.Test t = ts.createTest("abcdefghijkl", "900000000000", c, tt1, pc, par, data);
+    List <app.domain.model.Test> testList = new ArrayList<>();
+    testList.add(t);
+    LabCoordinatorStore lc = new LabCoordinatorStore();
+    Map <String, Integer> a = lc.getNumberTestsByDay(testList,data,data,"Covid");
+    assertEquals(lc.getNumberTestsByDay(testList,data,data,"Covid"),a);
+    Map <String, Integer> b = lc.getNumberTestsByWeek(testList,data,data,"Covid");
+    assertEquals(b,lc.getNumberTestsByWeek(testList,data,data,"Covid"));
+    Map <String, Integer> cc = lc.getNumberTestsByMonth(testList,data,data,"Covid");
+    assertEquals(lc.getNumberTestsByMonth(testList,data,data,"Covid"),cc);
+    Map <String, Integer> d = lc.getNumberTestsByYear(testList,data,data,"Covid");
+    assertEquals(d,lc.getNumberTestsByYear(testList,data,data,"Covid"));
+    }
+
+    @Test
+    public void testGetNumOfTReadyBy() {
         Client c = new Client(1234567890123456L, 1234567890, "22-01-2002", "jorge@gmail.com", 1111111111L, 22222222222L, "Jorge Ferreira");
         List<ParameterCategory> pc = new ArrayList<>();
         ParameterCategory p1 = new ParameterCategory("CAT00", "Category00");
@@ -30,55 +76,42 @@ public class LabCoordinatorStoreTest {
         List <app.domain.model.Test> testList = new ArrayList<>();
         testList.add(t);
         LabCoordinatorStore lc = new LabCoordinatorStore();
-        assertEquals(lc.getNumberOfTNR(testList),0);
-        List<app.domain.model.Test> test = new ArrayList<>();
-        t.setSamplesCollectionDate(data);
-        test.add(t);
-        assertEquals(lc.getNumberOfTNR(test),1);
-        assertEquals(lc.getNumberOfTR(testList),0);
-        List<app.domain.model.Test> test1 = new ArrayList<>();
-        t.setValidationDate(data);
-        test1.add(t);
-        assertEquals(lc.getNumberOfTR(test1),1);
-    }
-
-
- */
-
-
-
-    @Test
-    public void setInterval() {
-
+        assertEquals(lc.getNumOfTReadyByDay(testList,data,"clients"),1);
+        assertEquals(lc.getNumOfTReadyByDay(testList,data,"readyTests"),0);
+        assertEquals(lc.getNumOfTReadyByDay(testList,data,"missingResultsTests"),0);
+        assertEquals(lc.getNumOfTReadyByDay(testList,data,"diagnosisTests"),0);
+        assertEquals(lc.getNumOfTReadyByMonth(testList,data,"clients",data,data),1);
+        assertEquals(lc.getNumOfTReadyByMonth(testList,data,"diagnosisTests",data,data),0);
+        assertEquals(lc.getNumOfTReadyByMonth(testList,data,"missingResultsTests",data,data),0);
+        assertEquals(lc.getNumOfTReadyByMonth(testList,data,"readyTests",data,data),0);
+        assertEquals(lc.getNumOfTReadyByWeek(testList,data,"clients",data,data),1);
+        assertEquals(lc.getNumOfTReadyByWeek(testList,data,"diagnosisTests",data,data),0);
+        assertEquals(lc.getNumOfTReadyByWeek(testList,data,"missingResultsTests",data,data),0);
+        assertEquals(lc.getNumOfTReadyByYear(testList,data,"clients",data,data),1);
+        assertEquals(lc.getNumOfTReadyByYear(testList,data,"readyTests",data,data),0);
+        assertEquals(lc.getNumOfTReadyByYear(testList,data,"missingResultsTests",data,data),0);
+        assertEquals(lc.getNumOfTReadyByYear(testList,data,"diagnosisTests",data,data),0);
     }
 
     @Test
-    public void maxSubArraySum() {
-        int [] list = new int [10];
-        list [1] = 1;
-        list [2] = 3;
+    public void others() {
+        Client c = new Client(1234567890123456L, 1234567890, "22-01-2002", "jorge@gmail.com", 1111111111L, 22222222222L, "Jorge Ferreira");
+        List<ParameterCategory> pc = new ArrayList<>();
+        ParameterCategory p1 = new ParameterCategory("CAT00", "Category00");
+        pc.add(p1);
+        TestType tt1 = new TestType("BLT00", "Blood Test", "Venipuncture", pc);
+        TestStore ts = new TestStore();
+        List<Parameter> par = new ArrayList<>();
+        Parameter par1= new Parameter("HB000", "HB", "Haemoglobin", pc);
+        par.add(par1);
+        Calendar data = Calendar.getInstance();
+        app.domain.model.Test t = ts.createTest("abcdefghijkl", "900000000000", c, tt1, pc, par, data);
+        List <app.domain.model.Test> testList = new ArrayList<>();
+        testList.add(t);
         LabCoordinatorStore lc = new LabCoordinatorStore();
-        assertNotEquals(lc.maxSubArraySum(list),lc.maxSubArraySum(list));
+        LocalDateTime D = LocalDateTime.now();
+        assertNotEquals(D, LabCoordinatorStore.tCalendarToLDT(data));
+        assertEquals(1,LabCoordinatorStore.countNumber(2));
+    }
     }
 
-
-    @Test
-    public void isTestInInterval() {
-    }
-
-    @Test
-    public void tStringToCalendar() {
-    }
-
-    @Test
-    public void getMax() {
-    }
-
-    @Test
-    public void maxPlaces() {
-    }
-
-    @Test
-    public void countNumber() {
-    }
-}
