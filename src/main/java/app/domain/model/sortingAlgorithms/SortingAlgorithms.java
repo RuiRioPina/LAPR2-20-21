1599992c17cbe5ea868010ab74d6b3ec2a9f6407
RuiRioPina <1201568@isep.ipp.pt>
2021-06-20 21:13:56 +0100

    [PPROG] Finishing JavaDoc to my classes [US-13,US-19]

--- src/main/java/app/domain/model/sortingAlgorithms/SortingAlgorithms.java
+++ src/main/java/app/domain/model/sortingAlgorithms/SortingAlgorithms.java
@@ -5,5 +5,12 @@ import app.domain.model.Client;
 import java.util.List;
 
 public interface SortingAlgorithms {
+    /**
+     * The method to be overridden by sorting methods
+     * @param arrayNames names of the clients
+     * @param arrayTins tins of the clients
+     * @param clients clients in the company
+     * @param dummy just to differentiate from string to tin
+     */
     void sortMethod(List<String> arrayNames, List<Long> arrayTins, List<Client> clients, int dummy);
 }
